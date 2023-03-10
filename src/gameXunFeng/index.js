11a6c0d366073f65358071e0e24587e1142e524a
pang <liwenxinbz@163.com>
2023-03-10 23:13:21 +0800

    update

--- src/gameXunFeng/index.js
+++ src/gameXunFeng/index.js
@@ -96,6 +96,8 @@ export async function initXunFeng(bot) {
           '文创套装7',
           '文创套装8',
           '文创套装9',
+          '文创套装10',
+          '文创套装11',
         ].forEach(async (txtName) => {
           let contact =
               (await bot.Contact.find({name: txtName})) ||
